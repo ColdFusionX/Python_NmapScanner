79e292754bda9a177f90119e12f24da6688d9431
ColdFusionX <deshmukh.mayank@hotmail.com>
2019-03-10 00:17:55 +0530

    Create nmap.py

---
+++ nmap.py
@@ -0,0 +1,50 @@
+#!/usr/bin/python3  #can even use #!/usr/bin/env python (which detects the python versions itself)
+
+import nmap  #importing the nmap module
+
+scanner = nmap.PortScanner()  #creating variable named scanner and calling the portscanner class
+
+print("Welcome, this is a simple nmap automation tool")
+print("<----------------------------------------------------->")
+
+ip_addr = input("Please enter the IP address you want to scan: ") #new variable ip_addr
+print("The IP you entered is: ", ip_addr) 
+type(ip_addr) #specifying the type so string cannot be entered
+
+resp = input("""\nPlease enter the type of scan you want to run
+                1)SYN ACK Scan
+                2)UDP Scan
+                3)Comprehensive Scan
+                4)IP protocol ping \n""")  #selecting the specific scan as per user response
+print("You have selected option: ", resp)
+
+if resp == '1':
+    print("Nmap Version: ", scanner.nmap_version())  #calling additional functionality method using scanner variable
+    scanner.scan(ip_addr, '1-1024', '-v -sS')
+    print(scanner.scaninfo())
+    print("Ip Status: ", scanner[ip_addr].state())   #.state for getting the status of the host
+    print(scanner[ip_addr].all_protocols())
+    print("Open Ports: ", scanner[ip_addr]['tcp'].keys())  #.keys retunrs all active ports within the range
+elif resp == '2':
+    print("Nmap Version: ", scanner.nmap_version())
+    scanner.scan(ip_addr, '1-1024', '-v -sU')
+    print(scanner.scaninfo())
+    print("Ip Status: ", scanner[ip_addr].state())
+    print(scanner[ip_addr].all_protocols())
+    print("Open Ports: ", scanner[ip_addr]['udp'].keys())
+elif resp == '3':
+    print("Nmap Version: ", scanner.nmap_version())
+    scanner.scan(ip_addr, '1-1024', '-v -sS -sV -sC -A -O')
+    print(scanner.scaninfo())
+    print("Ip Status: ", scanner[ip_addr].state())
+    print(scanner[ip_addr].all_protocols())
+    print("Open Ports: ", scanner[ip_addr]['tcp'].keys())
+elif resp == '4':
+    print("Nmap Version: ", scanner.nmap_version())
+    scanner.scan(ip_addr, '1-1024', '-v -PO')
+    print(scanner.scaninfo())
+    print("Ip Status: ", scanner[ip_addr].state())
+    print(scanner[ip_addr].all_protocols())
+    print("Open Ports: ", scanner[ip_addr]['tcp'].keys())
+elif resp >= '5':
+print("Please enter a valid option")
